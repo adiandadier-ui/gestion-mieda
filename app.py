34843894ce0a166efb179506c90191aeb99483a6
adiandadier-ui <adiandadier@gmail.com>
2026-06-24 07:53:19 +0000

    Fix typo in ingredient list

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ MATIERES_PREMIERES_CIBLES = [
     "LAPIN", 
     "PINTADE",
     "PORTION SAUCE"
-    "LIQUEUR HAMWICK"
+    "LIQUEUR"
 ]
 
 def determiner_matiere_premiere(nom_article, prix_unitaire):
